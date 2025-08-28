a7aaa81d3257baf6f4c87bce290e887a0430587b
Anshu <80507004+ANSHUNP@users.noreply.github.com>
2025-08-28 22:26:25 +0530

    inital commit

---
+++ src/test/java/com/Anshu/EnotesApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Anshu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnotesApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
